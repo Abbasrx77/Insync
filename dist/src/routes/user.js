124cdb0dca118ad4da2b3095eb5e3af0177ce885
Abbasrx77 <abbasmama007@gmail.com>
2024-01-25 21:13:37 +0100

    Add basic config--patch

--- dist/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-router.route('/api/v1/users').get().post().patch().delete();
-//# sourceMappingURL=user.js.map
